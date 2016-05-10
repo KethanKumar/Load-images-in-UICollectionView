88f7c5950923811a2c05e657bea565e151763c79
Software <software@omni-udpmac29.local>
2016-05-10 16:23:45 +0530

    Initial Commit

---
+++ ImageLoad/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  ImageLoad
+//
+//  Created by Software on 10/05/16.
+//  Copyright © 2016 Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ ImageLoad/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  ImageLoad
+//
+//  Created by Software on 10/05/16.
+//  Copyright © 2016 Software. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
